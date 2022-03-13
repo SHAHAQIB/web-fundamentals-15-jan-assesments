ddadfae2c1a52b94b767d67969665721d16b2689
SHAHAQIB <95542593+SHAHAQIB@users.noreply.github.com>
2022-03-13 20:06:45 +0530

    Update mar-11.js

--- mar-11/mar-11.js
+++ mar-11/mar-11.js
@@ -1,40 +1,47 @@
-const convert_to_json = function(response) {
+let convert_json = function(response){
     return response.json();
-}
-
-
-
-const handle_trend =        function(res) {
-    const user_arr = res.data;
-    const container_img = document.getElementById('container');
-    for (var i = 0; i < res.data.length; i += 1){
-        const single = user_arr[i];
-        const new_img = document.createElement('img');
-        new_img.src = single.images.original.url;
-        container_img.appendChild(new_img)
-    }
-}
-
-fetch(`https://api.giphy.com/v1/gifs/trending?api_key=r9bhJQtiZEl478EwUQbGor3jdNCjbNv8`).then(convert_to_json).then(handle_trend);
-
-const handle_search = function(search_response) {
-
-    var image_el = document.getElementById('search-image');
-    var image_e = search_response.data[0].images.original.url;
-    var img_c = document.createElement("img");
-    img_c.setAttribute("src", image_e);
-    document.body.appendChild(img_c);
-    console.log(search_response.data[0].images.original.url);
-}
-
-const search_fn = function() {
-    
-
-    const input = document.getElementById('input-text');
-    const user_id = input.value;
-    fetch(`r9bhJQtiZEl478EwUQbGor3jdNCjbNv8=${user_id}`).then(convert_to_json).then(handle_search);
-}
-
-
-const btn = document.getElementById('search-btn');
-btn.addEventListener('click', search_fn);
+  }
+  
+  let get_data = function(res){
+      let arr = res.data;
+      let clg = document.getElementById("gifs");
+      for(let i = 0; i < arr.length; i++){
+          let new_elem = document.createElement("video")
+          new_elem.src = arr[i].images.looping.mp4;
+          new_elem.classList.add = "vdo";
+          new_elem.setAttribute("autoplay","");
+          new_elem.setAttribute("loop","");
+          clg.appendChild(new_elem);
+      }
+  
+  }
+  
+  fetch("https://api.giphy.com/v1/gifs/trending?api_key=4bAK73Z75erE2xk3awKlwTuLM5JNCfmL&limit=20").then(convert_json).then(get_data);
+  
+  
+  
+  const handle_search = function(res) {
+      let container = document.getElementById("gifs");
+      container.innerHTML = "";
+      let arr = res.data;
+      let clg = document.getElementById("gifs");
+      for(let i = 0; i < arr.length; i++){
+          let new_elem = document.createElement("video")
+          new_elem.src = arr[i].images.looping.mp4;
+          new_elem.classList.add = "vdo";
+          new_elem.setAttribute("autoplay","");
+          new_elem.setAttribute("loop","");
+          clg.appendChild(new_elem);
+      }
+  }
+  
+  const search_fn = function() {
+      const input = document.getElementById('input_field');
+      const user_id = input.value;
+      console.log(fetch(`https://api.giphy.com/v1/gifs/search?api_key=4bAK73Z75erE2xk3awKlwTuLM5JNCfmL&limit=20&q=${user_id}`)
+      .then(convert_json)
+      .then(handle_search));
+  }
+  
+  const btn = document.getElementById('search_btn');
+  btn.addEventListener('click', search_fn);
